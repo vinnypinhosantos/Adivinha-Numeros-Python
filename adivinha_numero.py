f4b82b5fe5842221eb215d9379caa39453d89689
vinnypinhosantos <vinnypinhosantos@gmail.com>
2022-03-07 17:23:22 -0300

    NÃO USAR WHILE
    Lembrete: nesse caso, usar o while ao invés do for cria um looping infinito (que queremos evitar).

--- adivinha_numero.py
+++ adivinha_numero.py
@@ -11,3 +11,13 @@ fim_intervalo = int(input("Digite o final do intervalo: "))
 numero_aleatorio = random.randint(inicio_intervalo, fim_intervalo)
 
 print(f"PENSEI EM UM NÚMERO ENTRE {inicio_intervalo} E {fim_intervalo}")
+
+palpite = int(input("Digite seu palpite: "))
+
+while (palpite != numero_aleatorio):
+    if palpite > numero_aleatorio:
+        print("ERROU! Digite um número menor")
+    elif palpite < numero_aleatorio:
+        print("ERROU! Digite um número maior")
+
+print(f"Você ACERTOU! Estava pensando no número {numero_aleatorio}")
