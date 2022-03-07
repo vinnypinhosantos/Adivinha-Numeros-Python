d8b67a543c10f06072d44cf0e21af9a8cf362edc
vinnypinhosantos <vinnypinhosantos@gmail.com>
2022-03-07 18:16:23 -0300

    DÁ PRA USAR O WHILE
    Descobri a melhor forma de forma de fazer o exercício proposto com o while, mas dessa vez utilizando um contador e um cálculo para definir o número de tentativas.

--- adivinha_numero.py
+++ adivinha_numero.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 print("OLÁ, IREI TESTAR A SUA INTELIGÊNCIA")
 print("Selecione um intervalo de números", end=" ")
@@ -9,15 +10,23 @@ inicio_intervalo = int(input("Digite o inicio do intervalo: "))
 fim_intervalo = int(input("Digite o final do intervalo: "))
 
 numero_aleatorio = random.randint(inicio_intervalo, fim_intervalo)
-
 print(f"PENSEI EM UM NÚMERO ENTRE {inicio_intervalo} E {fim_intervalo}")
 
+numero_tentativas = int(math.log(fim_intervalo - inicio_intervalo + 1, 2))
+print(f"Você tem {numero_tentativas} TENTATIVAS!")
+
 palpite = int(input("Digite seu palpite: "))
+contador_tentativas = 0
 
-while (palpite != numero_aleatorio):
+while (contador_tentativas != numero_tentativas):
     if palpite > numero_aleatorio:
+        contador_tentativas += 1
         print("ERROU! Digite um número menor")
+        palpite = int(input("Digite seu palpite: "))
     elif palpite < numero_aleatorio:
+        contador_tentativas += 1
         print("ERROU! Digite um número maior")
-
-print(f"Você ACERTOU! Estava pensando no número {numero_aleatorio}")
+        palpite = int(input("Digite seu palpite: "))
+    elif palpite == numero_aleatorio:
+        print(f"Você ACERTOU! Estava pensando no número {numero_aleatorio}")
+        break
